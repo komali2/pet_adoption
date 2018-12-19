550f34dc32fa14a368728c6a393bef5e06ec53b4
Caleb Rogers <rogersjcaleb@gmail.com>
2018-12-18 21:08:08 -0800

    Create function to generate li's with imgs in them

--- scripts.js
+++ scripts.js
@@ -1,15 +1,35 @@
 window.onload = function(){
     var request = new XMLHttpRequest();
-    request.addEventListener("load", handleDogsResponse);
-    request.open("GET", "/assets/data/dogs.json");
+    request.addEventListener('load', handleDogsResponse);
+    request.open('GET', '/assets/data/dogs.json');
     request.send();
 };
 
 function handleDogsResponse(){
     if(this.status === 200) {
         // Success
-        var dogs_array = JSON.parse(this.responseText);
+        var dogs_array = JSON.parse(this.responseText).dogs;
+        var li = buildListImageElement(dogs_array[0]);
+        var dogs_list_ul = document.getElementById('dogs-list');
+        dogs_list_ul.appendChild(li);
     } else {
         // Error
     }
+}
+
+function buildListImageElement(obj){
+    var li = document.createElement('li');
+    var img = document.createElement('img');
+    img.setAttribute('src', obj.image);
+    img.setAttribute('alt', 
+        'An adorable doggy available for adoption, source image from ' + 
+        obj.source);
+    /**
+     * One might be tempted to add a "title" attribute here, to allow for 
+     * tooltip hovering. That's not a very good idea, and this article has a 
+     * great explanation as to why:
+     * https://developer.paciellogroup.com/blog/2013/01/using-the-html-title-attribute-updated/
+     */
+    li.appendChild(img);
+    return li;
 }
