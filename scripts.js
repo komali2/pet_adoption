43aea87a5cad0289c7aaa9941c925e8e92d72469
Caleb Rogers <rogersjcaleb@gmail.com>
2018-12-18 20:19:18 -0800

    Grab dogs array
    This could later be converted to an external API call

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 window.onload = function(){
-    // Main code goes here
+    var request = new XMLHttpRequest();
+    request.addEventListener("load", handleDogsResponse);
+    request.open("GET", "/assets/data/dogs.json");
+    request.send();
 };
+
+function handleDogsResponse(){
+    if(this.status === 200) {
+        // Success
+        var dogs_array = JSON.parse(this.responseText);
+    } else {
+        // Error
+    }
+}
